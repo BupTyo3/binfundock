407c3193d07ca0b93f979d005d6103e19dd11628
Artem Poliakov <art_polyakov@mail.ru>
2020-10-13 18:52:10 +0300

    Add SignalHistory model; Add all_targets field

--- apps/signal/base_model.py
+++ apps/signal/base_model.py
@@ -1,8 +1,9 @@
 import logging
 
-from typing import Optional, TYPE_CHECKING
+from abc import abstractmethod
+from typing import Optional, TYPE_CHECKING, Union
 
-from utils.framework.models import SystemBaseModel
+from utils.framework.models import SystemBaseModel, SystemBaseModelWithoutModified
 from .utils import SignalStatus
 
 if TYPE_CHECKING:
@@ -26,5 +27,24 @@ class BaseSignal(SystemBaseModel):
     @status.setter
     def status(self, value):
         logger.debug(f'Set Signal status: {self}: {self._status.upper()} -> {value.upper()}')
+        BaseHistorySignal.write_in_history(signal=self, status=value)
         self._status = value
 
+
+class BaseHistorySignal(SystemBaseModelWithoutModified):
+
+    class Meta:
+        abstract = True
+
+    def save(self, *args, **kwargs):
+        """Disable update"""
+        if self.pk is None:
+            super().save(*args, **kwargs)
+
+    @classmethod
+    @abstractmethod
+    def write_in_history(cls,
+                         signal: BaseSignal,
+                         status: str,
+                         current_price: Optional[float] = None):
+        pass
