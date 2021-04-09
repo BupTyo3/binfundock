b665a2c8fa5f2c767975496e8af2250bdfdd16d2
Eugene.Povetkin <Eugene.Povetkin@cloudbeds.com>
2021-04-09 10:13:58 +0300

    change time to retry of cancelling

--- apps/signal/admin.py
+++ apps/signal/admin.py
@@ -264,12 +264,12 @@ class SignalAdmin(admin.ModelAdmin):
     def _sell_by_market_one(self, request, signal):
         counter = 1
         cancelled_signal = False
-        while not cancelled_signal and counter < 601:
+        while not cancelled_signal and counter < 301:
             logger.info(f'Trying to close the signal: {signal.symbol}, id:{signal.id}, Attempt #{counter}')
             signal.try_to_spoil_by_one_signal(True)
             cancelled_signal = Signal.objects.filter(id=signal.id, _status__in=CANCELING__SIG_STATS).exists()
             logger.info(f'Is signal {signal.symbol} with id:{signal.id} cancelled: {cancelled_signal}')
-            time.sleep(0.3)
+            time.sleep(1)
             counter += 1
 
     @notifications_handling('')
